57f35f6db36ee5588dc9d2a3b0718c42637452e6
iuq276 <iuq276@my.utsa.edu>
2020-02-16 23:32:08 -0600

    Assignment 1 done

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, jsonify, abort, make_response
 from flask import request
-#from flask_mysqldb import MySQL
-from flask_sqlalchemy import SQLAlchemy
-import pymysql
+
 import MySQLdb as mdb
-from flaskext.mysql import MySQL
-import jaydebeapi
 
-
-app = Flask(__name__) #create the Flask app
+app = Flask(__name__)  # create the Flask app
 
 db = mdb.connect("129.115.27.67", "iuq276", "kyYstgM5lpci8YaCxT4R", "iuq276")
 
@@ -16,24 +11,27 @@ curs = db.cursor()
 
 app.config["JSON_SORT_KEYS"] = False
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 
 @app.route('/hello')
 def hello():
     return '[{"message":"hello yourself"}]'
 
-@app.route('/properties',  methods=['GET', 'POST'])
+
+@app.route('/properties', methods=['GET', 'POST'])
 def props():
     if request.method == 'GET':
-        #curs = db.cursor()
+        # curs = db.cursor()
         try:
             curs.execute("SELECT * FROM usprops")
             rv = curs.fetchall()
             l = []
             for result in rv:
-                content = {'id': result[0], 'address': result[1], 'zip': result[2]}
+                content = {'id': result[0], 'address': result[1], 'city': result[2], 'state': result[3], 'zip': result[4]}
                 l.append(content)
 
             return jsonify(l)
@@ -43,11 +41,54 @@ def props():
         curs.close()
         db.close()
 
-    else:
-        return "man"
+    elif request.method == 'POST':
+        data = request.get_json()
+        addr = data['address']
+        city = data['city']
+        state = data['state']
+        zip_code = data['zip']
 
+        try:
+            query = "INSERT INTO usprops(address, city, state, zip) values (%s, %s, %s, %s)"
+            args = (addr, city, state, zip_code)
+            curs.execute(query, args)
+            db.commit()
+            return "added"
+        except:
+            return "something went wrong"
+        curs.close()
+        db.close()
+
+    else:
+        return "NOT GET OR POST"
+
+
+@app.route('/properties/<req_id>', methods=['GET', 'DELETE'])
+def get_id(req_id):
+    if request.method == 'GET':
+        try:
+            query = "SELECT * FROM usprops where id = %s"
+            curs.execute(query, req_id)
+
+            rv = curs.fetchall()
+            l = []
+            for result in rv:
+                content = {'id': result[0], 'address': result[1], 'city': result[2], 'state': result[3], 'zip': result[4]}
+                l.append(content)
+
+            return jsonify(l)
+
+        except:
+            return "Error, unable to fetch properties"
+    elif request.method == 'DELETE':
+        try:
+            query = "DELETE FROM usprops where id = %s"
+            curs.execute(query, req_id)
+            db.commit()
+            return "deleted successfully"
+        except:
+            return "something went wrong"
 
 
 if __name__ == '__main__':
-
     app.run()
