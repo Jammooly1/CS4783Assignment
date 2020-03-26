f61ac29ed3b2414fb017c5eeded38e8b2438da8f
majdhamoudah <42590417+majdhamoudah@users.noreply.github.com>
2020-03-26 17:49:43 -0500

    updates CORS

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
+import flask_cors
 import pymysql
-from flask import Flask, jsonify, abort, Response
+from flask import Flask, jsonify, abort, Response, send_from_directory
 from flask import request
 from flask_sslify import SSLify
 from flask_swagger_ui import get_swaggerui_blueprint
 import pymysql.cursors
 import sys
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
+# from routes import register_views
 app = Flask(__name__)  # create the Flask app
-CORS(app)
 context = ('web.crt', 'web.key')
-#sslify = SSLify(app)
+
+
+# sslify = SSLify(app)
 
 # db = mdb.connect("129.115.27.67", "iuq276", "kyYstgM5lpci8YaCxT4R", "iuq276")
-db = pymysql.connect(host='129.115.27.67', user='iuq276', password='kyYstgM5lpci8YaCxT4R', db='iuq276',
-                     charset='utf8mb4',
-                     cursorclass=pymysql.cursors.DictCursor
-                     )
-curs = db.cursor()
+def connect_db():
+    db = pymysql.connect(host='129.115.27.67', user='iuq276', password='kyYstgM5lpci8YaCxT4R', db='iuq276',
+                         charset='utf8mb4',
+                         cursorclass=pymysql.cursors.DictCursor
+                         )
+    curs = db.cursor()
+    return db, curs
+
 
 app.config["JSON_SORT_KEYS"] = False
 
+
 ### swagger specific ###
+@app.route('/static/<path:path>')
+@cross_origin()
+def send_static(path):
+    return send_from_directory('static', path)
+
+
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
@@ -37,20 +50,24 @@ app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 ### end swagger specific ###
 
 @app.route('/')
+@cross_origin()
 def hello_world():
     return 'Hello World!'
 
 
 @app.route('/hello')
+@cross_origin()
 def hello():
     return '[{"message":"hello yourself"}]'
 
 
 @app.route('/properties', methods=['GET', 'POST'])
+@cross_origin()
 def props():
     if request.method == 'GET':
         # curs = db.cursor()
         try:
+            db, curs = connect_db()
             curs.execute("SELECT * FROM usprops")
             rv = curs.fetchall()
 
@@ -72,6 +89,8 @@ def props():
             zip_code = data['zip']
 
             try:
+                db, curs = connect_db()
+
                 query = "INSERT INTO usprops(address, city, state, zip) values (%s, %s, %s, %s)"
                 args = (addr, city, state, zip_code)
                 curs.execute(query, args)
@@ -89,11 +108,14 @@ def props():
 
 
 @app.route('/properties/<req_id>', methods=['GET', 'DELETE', 'PUT'])
+@cross_origin()
 def get_id(req_id):
     if request.method == 'GET':
         if not req_id.isdigit():
             return Response("ID IS NOT A NUMBER", status=404, mimetype="application/json")
         try:
+            db, curs = connect_db()
+
             query = "SELECT * FROM usprops where id = %s"
             curs.execute(query, req_id)
 
@@ -111,6 +133,8 @@ def get_id(req_id):
             if not req_id.isdigit():
                 abort(400)
             try:
+                db, curs = connect_db()
+
                 query = "DELETE FROM usprops where id = %s"
                 curs.execute(query, req_id)
                 db.commit()
@@ -132,6 +156,8 @@ def get_id(req_id):
             state = data['state']
             zip_code = data['zip']
 
+            db, curs = connect_db()
+
             query = "SELECT * FROM usprops where id = %s"
             curs.execute(query, req_id)
 
@@ -147,6 +173,8 @@ def get_id(req_id):
                 zip_code = rv.get('zip')
 
             try:
+                db, curs = connect_db()
+
                 query = "Update usprops set address = %s, city =%s, state = %s, zip = %s where id = %s"
                 args = (addr, city, state, zip_code, req_id)
                 curs.execute(query, args)
@@ -161,8 +189,10 @@ def get_id(req_id):
 
 
 if __name__ == '__main__':
+    # register_views(app)
+    CORS(app, resources={r"*": {"origins": "*"}}, supports_credentials=True)
     if sys.argv[1] == 'https' or sys.argv[1] == 'Https':
-        #context = ('web.crt', 'web.key')
+        # context = ('web.crt', 'web.key')
         app.run(host="0.0.0.0", port=12100, ssl_context='adhoc')
     elif sys.argv[1] == 'http' or sys.argv[1] == 'HTTP':
         app.run(host="0.0.0.0", port=12100)
